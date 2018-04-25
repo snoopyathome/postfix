df73d55ddc4ea113be0a71cf19c1d15bf1844adb
snoopyathome <mail-an-muehlbauer@web.de>
2018-04-25 15:35:33 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 rsyslogd
-rm /etc/postfix/transport*
+rm /etc/aliases* /etc/postfix/transport*
 cp /defaults/* /etc/postfix/
 sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/postfix/main.cf
 echo -e $MYNETWORKS$RELAYDOMAINS >> /etc/postfix/main.cf
+touch /etc/aliases
+postmap /etc/aliases
 /usr/sbin/postfix -c /etc/postfix start
 tail -F /var/log/maillog
