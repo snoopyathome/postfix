fc56c9ad79fa8bcc32139d011961eb1c56124483
snoopyathome <mail-an-muehlbauer@web.de>
2018-12-30 19:57:01 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -5,6 +5,6 @@ cp /defaults/*.cf /etc/postfix/
 cp /configuration/aliases /etc/aliases
 sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/postfix/main.cf
 echo -e $MYNETWORKS >> /etc/postfix/main.cf
-postmap /etc/aliases
+postalias /etc/aliases
 /usr/sbin/postfix -c /etc/postfix start
 tail -F /var/log/maillog
