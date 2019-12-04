fa13f4ea56b3ee1b251641bf327694c4fa4297a6
snoopyathome <mail-an-muehlbauer@web.de>
2019-12-04 16:51:33 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 rsyslogd
 rm /etc/aliases* /etc/postfix/transport*
-cp /defaults/*.cf /etc/postfix/
-/configuration/users.sh
-cp /configuration/aliases /etc/aliases
+cp /defaults/* /etc/postfix/
 sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/postfix/main.cf
 sed -i "s/DESTINATION/${DESTINATION}/g" /etc/postfix/main.cf
 echo -e $MYNETWORKS >> /etc/postfix/main.cf
-postalias /etc/aliases
+postalias /etc/postfix/virtual
 /usr/sbin/postfix -c /etc/postfix start
 tail -F /var/log/mail.log
