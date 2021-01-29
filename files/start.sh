4779751aeb44562c0a3e75e87c656907013c40f4
snoopyathome <mail-an-muehlbauer@web.de>
2021-01-29 14:10:19 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-rsyslogd
 rm /etc/postfix/transport*
 cp /defaults/* /etc/postfix/
 sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/postfix/main.cf
 sed -i "s/DESTINATION/${DESTINATION}/g" /etc/postfix/main.cf
 echo -e $MYNETWORKS >> /etc/postfix/main.cf
 postalias /etc/postfix/virtual
-/usr/sbin/postfix -c /etc/postfix start
-tail -F /var/log/mail.log
+/usr/sbin/postfix -c /etc/postfix start-fg
