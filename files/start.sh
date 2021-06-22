e731c741b2a8bf8270a51980cdda825bfa4fbd12
snoopyathome <mail-an-muehlbauer@web.de>
2021-06-22 15:30:53 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -2,7 +2,6 @@
 rm /etc/postfix/transport*
 cp /defaults/* /etc/postfix/
 sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/postfix/main.cf
-sed -i "s/DESTINATION/${DESTINATION}/g" /etc/postfix/main.cf
 sed -i "s/VIRTUALMAILBOXDOMAINS/${VIRTUALMAILBOXDOMAINS}/g" /etc/postfix/main.cf
 echo -e $MYNETWORKS >> /etc/postfix/main.cf
 postalias /etc/postfix/virtual
